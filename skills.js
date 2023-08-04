31727cef9098c97112d72467748e19d347b9da0b
julianasf-91 <62219546+julianasf-91@users.noreply.github.com>
2023-08-04 13:16:41 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,8 @@
+function calculateNumbers(var1, var2) {
+for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+            
+    }
+}
+}
